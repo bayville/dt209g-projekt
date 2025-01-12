af7ef49f7117ebf758a5ba0e2f98e44e05d961e7
Fredrik <fredrik@bayville.se>
2025-01-12 08:37:09 +0100

    Added map widget

---
+++ patterns/map-widget.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Title: Map Widget
+ * Slug: gefle-workspace/map-widget
+ */
+?>
+
+<!-- wp:group {"className":"widget__container map-widget","layout":{"type":"constrained"}} -->
+<div class="wp-block-group widget__container map-widget"><!-- wp:heading {"className":"text-xl mb-xs"} -->
+  <h2 class="wp-block-heading text-xl mb-xs">Hitta hit</h2>
+  <!-- /wp:heading -->
+
+  <!-- wp:wpgmza-standalone/map-block {"lat":"60.6736229635101","lng":"17.13722202416339","zoom":17,"height":"20rem"} /-->
+</div>
+<!-- /wp:group -->
